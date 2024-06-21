d7d25f7733867df8e9dab914bce4cbb81f57e24f
Lindurz234 <234ababil@gmail.com>
2024-06-22 00:56:10 +0700

    Update token2.sol

--- token2.sol
+++ token2.sol
@@ -42,9 +42,9 @@ library SafeMath {
 contract NEWBSCCONTRACT {
     using SafeMath for uint256;
  
-    string public name = "TOKEN NAME";
-    string public symbol = "TOKEN";
-    uint256 public totalSupply = 1000000000000000000000000;
+    string public name = "Maganomics";
+    string public symbol = "MAGANOMICS";
+    uint256 public totalSupply =4961000000000000000000000000;
     uint8 public decimals = 18;
  
     mapping(address => uint256) public balanceOf;
