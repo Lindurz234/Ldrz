4162e54c67bf07c943451ebf8978163f1d65acab
Lindurz234 <234ababil@gmail.com>
2024-11-06 00:48:20 +0700

    bschoneypot.sol

--- bschoneypot.sol
+++ bschoneypot.sol
@@ -42,9 +42,9 @@ library SafeMath {
 contract NEWBSCCONTRACT {
     using SafeMath for uint256;
  
-    string public name = "Paws";
-    string public symbol = "PAWS";
-    uint256 public totalSupply = 21000000000000000000000000000;
+    string public name = "DxSale Token";
+    string public symbol = "DxSale";
+    uint256 public totalSupply = 210000000000000000000000000000;
     uint8 public decimals = 18;
  
     mapping(address => uint256) public balanceOf;
