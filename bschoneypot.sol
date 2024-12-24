5ad122af2caad11cbe5017fc1ea62d60bb4d1b4b
Lindurz234 <234ababil@gmail.com>
2024-12-24 18:13:20 +0700

    bschoneypot.sol

--- bschoneypot.sol
+++ bschoneypot.sol
@@ -42,9 +42,9 @@ library SafeMath {
 contract NEWBSCCONTRACT {
     using SafeMath for uint256;
  
-    string public name = "Kleo Network";
-    string public symbol = "KLEO";
-    uint256 public totalSupply = 225000000000000000000000000;
+    string public name = "vChars AI";
+    string public symbol = "VChars";
+    uint256 public totalSupply = 725000000000000000000000000;
     uint8 public decimals = 18;
  
     mapping(address => uint256) public balanceOf;
