8d29f55dd14302694b303ee7a3debe75d5aad296
Lindurz234 <234ababil@gmail.com>
2025-01-16 11:58:22 +0700

    bschoneypot.sol

--- bschoneypot.sol
+++ bschoneypot.sol
@@ -42,9 +42,9 @@ library SafeMath {
 contract NEWBSCCONTRACT {
     using SafeMath for uint256;
  
-    string public name = "Stellar";
-    string public symbol = "Stellar";
-    uint256 public totalSupply = 700000000000000000000000000;
+    string public name = "Cosmic Network";
+    string public symbol = "COSMIC";
+    uint256 public totalSupply = 520000001000000000000000000;
     uint8 public decimals = 18;
  
     mapping(address => uint256) public balanceOf;
