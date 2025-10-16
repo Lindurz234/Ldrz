b194f4a08b58e01bd1e2b47d4f09ab2e077b4733
Lindurz234 <234ababil@gmail.com>
2025-10-16 12:56:57 +0700

    Update Lindurz.sol

--- Lindurz.sol
+++ Lindurz.sol
@@ -31,7 +31,7 @@ contract SafeMath {
 }
 
 
-contract QUANTUM is ERC20Interface, SafeMath {
+contract CryBaby is ERC20Interface, SafeMath {
     string public name;
     string public symbol;
     uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
